5f518da78b139c2d2ec0043041bbd913cb101d16
renn1805 <renan.almeida.arau@gmail.com>
2026-02-07 02:52:34 -0300

    implementação do prismaLibSql

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from '../generated/prisma';
 import 'dotenv/config';
 
 // O segredo está aqui: passamos a URL dentro de um objeto, como o erro sugeriu
 const dbUrl = process.env.DATABASE_URL || 'file:/app/data/database.db';
 
-const adapter = new PrismaBetterSqlite3({
+const adapter = new PrismaLibSql({
     url: dbUrl
 });
 
