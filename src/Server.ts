1d95a005953611f1b462ea0c0e0bccf8d6659d6d
renn1805 <renan.almeida.arau@gmail.com>
2026-02-07 02:36:43 -0300

    p

--- src/Server.ts
+++ src/Server.ts
@@ -4,7 +4,7 @@ import routes from "./routes"
 
 app.use(routes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
 
 const smallerId = 7
